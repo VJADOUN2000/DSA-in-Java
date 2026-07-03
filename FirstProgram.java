910f5215bef810be09158d84525b3bd993663581
Vikrant Singh <jadounvikrant@gmail.com>
2026-07-03 23:52:51 +0530

    Initial commit - Java DSA setup

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+class FirstProgram{
+
+    public static void main(String[] args) {
+        System.out.println("Start of Java in DSA");
+
+        // this is is used for Comment or information without affecting the code
+
+        
+    }
+}
