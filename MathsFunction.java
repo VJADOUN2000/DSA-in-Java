a107883559b1a712f7b282182f5769476ee826f7
Vikrant Singh <jadounvikrant@gmail.com>
2026-07-07 22:43:13 +0530

    Variables, static and dynamic meaning

---
+++ MathsFunction.java
@@ -0,0 +1,10 @@
+class MathsFunction{
+    public static void main(String args[]){
+        System.out.println(Math.abs(45.25));
+    }
+}
+
+// What is static type -> the language in which variables are defined on compile time like Java, C++ 
+
+// what is dynamic type -> the language in which variables are defined on runtime like Python , JavaScript
+
